7c55456d1c91e645a8bbe7da17f448e6a83f4fed
Jacobo Tarrio <jtarrio@gmail.com>
2025-04-21 12:33:54 -0400

    Add missing copyright statements

--- src/dsp/buffers.ts
+++ src/dsp/buffers.ts
@@ -1,7 +1,21 @@
+// Copyright 2024 Jacobo Tarrio Barreiro. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /**
  * A source of pre-allocated arrays of a given size.
  */
-class Buffer<T extends ArrayLike<number>> {
+abstract class Buffer<T extends ArrayLike<number>> {
   /**
    * @param make A function that returns an array of the given length.
    * @param count The number of buffers to keep around. Having more than 1 lets you modify one buffer while you use another.
